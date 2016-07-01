83abee4193fd9d0228a506ba7ae0f25685398b0c
qtgye <buquia.jace@gmail.com>
2016-07-01 12:06:11 +0800

    typo fix

--- js/cloneObject.js
+++ js/cloneObject.js
@@ -1,7 +1,7 @@
 var cloneObject = function(obj) {
   var newObject = {};
   for ( var key in obj ) {
-	  newObj[key] = Object.prototype.toString.call(obj[key]).match(/Object/) ?
+	  newObject[key] = Object.prototype.toString.call(obj[key]).match(/Object/) ?
                   cloneObject(obj[key]) :
                   obj[key];
   }

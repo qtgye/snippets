e3365a904b49b42f5eb294d544c7a590203f1812
qtgye <buquia.jace@gmail.com>
2016-07-25 16:06:28 +0800

    Create elementState.js
    jQuery plugin

---
+++ js/elementState.js
@@ -0,0 +1,66 @@
+/**
+ *  Element State jQuery plugin
+ *	Allows the use of data-state attribute instead of classes for declaring UI states
+ *
+ * METHODS:
+ * 	- hasState		: checks whether an element is at a given state
+ * 	- addState		: adds a state to an element
+ * 	- removeState	: removes a state from an element
+ * 	- toggleState	: toggles a state from an element
+ *
+ * EVENTS:
+ * 	- statechange	: if a state has been added/removed from an element
+ */
+
+
+
+
+// hasState
+
+$.fn.hasState = function (state) {
+	var attr = this.attr('data-state');
+	if ( attr ) {
+		return attr.match(new RegExp('\\s?'+state+'\\s?')) ? true : false;
+	}
+	return false;
+	
+}
+
+
+
+// addState
+
+$.fn.addState = function (state) {
+	if ( !this.hasState(state) ) {
+		var currentState = this.attr('data-state');
+		currentState = currentState ? currentState.trim() : '';
+		this.attr('data-state',currentState+' '+state);
+		this.trigger('statechange',{ method : 'addState', targetState : state });
+	}
+	return this;
+}
+
+
+
+// removeState
+
+$.fn.removeState = function (state) {
+	if ( this.hasState(state) ) {
+		this.attr('data-state',this.attr('data-state').replace(state,''));
+		this.trigger('statechange',{ method : 'removeState', targetState : state });
+	}
+	return this;
+}
+
+
+
+// toggleState
+
+$.fn.toggleState = function (state) {
+	if ( this.hasState(state) ) {
+		this.removeState(state);
+	} else {
+		this.addState(state);
+	}
+	return this;
+}
